2f862e7364949bd9c04f44ff0ec1d4c37cac8fca
Joao Baumotte <jbaumotte@gmail.com>
2021-03-08 18:20:56 -0300

    New and create working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,8 +4,26 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+     flash[:notice] = 'Project added!'
+     redirect_to projects_path
+   else
+     flash[:error] = 'Failed to edit product!'
+     render :new
+   end
   end
 
   def destroy
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :path, :title)
   end
 end
